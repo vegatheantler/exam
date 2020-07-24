8e6a0e704f8f28397f9c2bd39ad735e6df4c469d
Abry <vega.antler@gmail.com>
2020-07-25 02:24:03 +0800

    Model Updated.

--- api/models/accountModel.js
+++ api/models/accountModel.js
@@ -4,22 +4,19 @@ var Schema = mongoose.Schema;
 
 
 var AccountSchema = new Schema({
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  BettingProfileId: {
+  CustomerId: {
     type: Number,
   },
-  currency: {
+  FirstName: {
+    type: String,
+  },
+  LastName: {
+    type: String,
+  },
+  PinCode: {
+    type: String,
+  },
+  ProfileId: {
     type: Number,
   },
 });

fbafd0457f372d5de505caca7207cf89a919deec
Abry <vega.antler@gmail.com>
2020-07-24 22:46:51 +0800

    route change

--- api/routes/accountRoutes.js
+++ api/routes/accountRoutes.js
@@ -2,6 +2,6 @@
 module.exports = function(app) {
   var account = require('../controllers/accountController');
 
-  app.route('/AddAccount')
+  app.route('/addaccount')
     .post(account.add_account);
 };
